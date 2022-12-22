c6d322c0282dea53335743199443225ce4d05879
rudzzz <rudsolza13@gmail.com>
2022-12-21 23:18:52 -0300

    code

---
+++ src/app/Program.java
@@ -0,0 +1,29 @@
+package app;
+
+import entities.Financing;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		try {
+			Financing f1 = new Financing(100000.0, 2000.0, 20);
+			System.out.println("Financial data 1:");
+			System.out.printf("Income: %.2f%n", f1.entry());
+			System.out.printf("Share: %.2f%n", f1.quota());
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Error in financial 1: " + e.getMessage());
+		}
+		
+		try {
+			Financing f2 = new Financing(100000.0, 2000.0, 80);
+			System.out.println("Financial data 2:");
+			System.out.printf("Income: %.2f%n", f2.entry());
+			System.out.printf("Share: %.2f%n", f2.quota());
+		}
+		catch (IllegalArgumentException e) {
+			System.out.println("Error in financial 2: " + e.getMessage());
+		}
+	}
+}
