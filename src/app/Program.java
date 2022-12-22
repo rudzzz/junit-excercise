dbca343d60f2a36acddb56b44212223982bbccd3
rudzzz <rudsolza13@gmail.com>
2022-12-22 07:36:19 -0300

    code

--- src/app/Program.java
+++ src/app/Program.java
@@ -10,7 +10,7 @@ public class Program {
 			Financing f1 = new Financing(100000.0, 2000.0, 20);
 			System.out.println("Financial data 1:");
 			System.out.printf("Income: %.2f%n", f1.entry());
-			System.out.printf("Share: %.2f%n", f1.quota());
+			System.out.printf("Quota: %.2f%n", f1.quota());
 		}
 		catch (IllegalArgumentException e) {
 			System.out.println("Error in financial 1: " + e.getMessage());
@@ -20,7 +20,7 @@ public class Program {
 			Financing f2 = new Financing(100000.0, 2000.0, 80);
 			System.out.println("Financial data 2:");
 			System.out.printf("Income: %.2f%n", f2.entry());
-			System.out.printf("Share: %.2f%n", f2.quota());
+			System.out.printf("Quota: %.2f%n", f2.quota());
 		}
 		catch (IllegalArgumentException e) {
 			System.out.println("Error in financial 2: " + e.getMessage());
